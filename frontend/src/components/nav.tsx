429a62c46c924cf3483c5ab2768e1bf9d074fbad
Vasil Lozanov <vasillozanov1@gmail.com>
2024-02-01 10:55:51 +0200

    added tailwindcss

---
+++ frontend/src/components/nav.tsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+
+    return (
+        <nav className="bg-gray-600">
+            <ul></ul>
+            <ul>
+                <li>Login</li>
+                <li>Logout</li>
+            </ul>
+        </nav>
+    )
+}

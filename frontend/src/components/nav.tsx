f3f23590140933578049509bf14159c3f4110105
Vasil Lozanov <vasillozanov1@gmail.com>
2024-02-06 17:38:58 +0200

    minor nav styling change

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -1,7 +1,7 @@
 export default function Nav() {
 
     return (
-        <nav className="bg-gray-700 flex justify-between">
+        <nav className="bg-slate-800 flex justify-around">
             <ul></ul>
             <ul className="flex gap-2">
                 <LinkItem href="#" label="Sign in" />
@@ -17,7 +17,7 @@ function LinkItem({ href, label }: { href: string, label: string }) {
     return (
         <li className="contents">
             <a
-                className="px-4 py-2 font-semibold text-white hover:bg-gray-500"
+                className="px-4 py-2 font-semibold text-white hover:bg-slate-700"
                 href={href}
             >
                 {label}

901f4f1714932c92036fb96b279893ff9363cf67
Vasil Lozanov <vasillozanov1@gmail.com>
2024-02-13 22:23:05 +0200

    extracted the input

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,13 +1,28 @@
-export type InputWithLabelProps = {
-    label: string
+export type InputProps = {
     value: string
+    type?: string
+}
+
+export type InputWithLabelProps = InputProps & {
+    label: string
 }
 
 export function InputWithLabel({ label, value }: InputWithLabelProps) {
 
     return (
-        <label className=""> {label}
-            <input className="rounded" value={value} />
+        <label className="flex gap-2"> {label}
+            <Input value={value} />
         </label>
+    )
+}
+
+export function Input({ value, type }: InputProps) {
+
+    return (
+        <input
+            className="rounded bg-slate-600 px-1"
+            value={value}
+            type={type}
+        />
     )
 }

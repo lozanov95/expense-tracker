6415db058de11e98861c53c4b6fc2b51229f3f15
Vasil Lozanov <vasillozanov1@gmail.com>
2024-02-15 18:17:37 +0200

    added onchange fn

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,28 +1,32 @@
+import { ChangeEventHandler, HTMLInputTypeAttribute } from "react"
+
 export type InputProps = {
-    value: string
-    type?: string
+    value: string | number
+    type?: HTMLInputTypeAttribute
+    onChange?: ChangeEventHandler<HTMLInputElement>
 }
 
 export type InputWithLabelProps = InputProps & {
     label: string
 }
 
-export function InputWithLabel({ label, value }: InputWithLabelProps) {
+export function InputWithLabel({ label, value, type, onChange }: InputWithLabelProps) {
 
     return (
         <label className="flex gap-2"> {label}
-            <Input value={value} />
+            <Input value={value} type={type} onChange={onChange} />
         </label>
     )
 }
 
-export function Input({ value, type }: InputProps) {
+export function Input({ value, type, onChange }: InputProps) {
 
     return (
         <input
             className="rounded bg-slate-600 px-1"
             value={value}
             type={type}
+            onChange={onChange}
         />
     )
 }
